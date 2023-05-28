80e13bad20aae568e90049d4b4b55cc81a462347
Nived Amin <134837491+nivedamin@users.noreply.github.com>
2023-05-28 17:51:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu 🥣 ')
+streamlit.text('Omega 3 & Blueberry Oatmeal 🥗')
+streamlit.text('Kale, Spinach & Rocket Smoothie 🥑')
+streamlit.text('Hard-Boiled Free-Range Egg 🐔')
