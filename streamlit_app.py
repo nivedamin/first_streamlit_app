0d914a436c570110848927229367aea0329645e3
Nived Amin <134837491+nivedamin@users.noreply.github.com>
2023-06-06 00:06:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,16 +41,16 @@ try:
     
     streamlit.header("The fruit list contains:")
     #Snowflake-related functions
-  def get_fruit_load_list():
-    with my_cnx_cur() as my_cur:
-      my_cur.execute("select * from fruit_load_list")
-      return my_cur.fetchall()
+def get_fruit_load_list():
+  with my_cnx_cur() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
       
-      #add a button to load the script
-      if streamlit.button('Get Fruit Load List'):
-        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-        my_data_rows = get_fruit_load_list()
-        streamlit.dataframe(my_data_rows)
+#add a button to load the script
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
         
 def insert_row_snowflake(new_fruit):
   with my_cnx_cur() as my_cur:
